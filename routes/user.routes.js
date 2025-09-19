b305880c1184879d70183c346eea62fc2313e980
Samuel Hope <samuelhope805@gmail.com>
2025-09-19 19:09:02 +0300

    first commit

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const ctrl = require('../controllers/user.controller');
 //const auth = require('../middleware/auth');
 const adminOnly = require('../middleware/adminOnly');
+const userController = require('../controllers/userController');
+const { userRegisterValidator } = require('../middleware/validators');
 
 router.get('/email/:email', ctrl.getByEmail);
 router.get('/:id',  adminOnly, ctrl.getById);
@@ -10,5 +12,6 @@ router.put('/:name', ctrl.update);
 router.delete('/:id', adminOnly, ctrl.remove);
 router.post('/createByAdmin',  adminOnly, ctrl.createByAdmin);
 router.post('/updatePassword',  ctrl.updatePassword);
+router.post('/register', userRegisterValidator, userController.registerUser);
 
 module.exports = router;
