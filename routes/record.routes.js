9d6cc20f68c6c67a19b1c8ce7b569fd0df5753bb
malexchem <malexchemsupplies@gmail.com>
2025-11-14 13:34:13 +0300

    Update record.routes.js

--- routes/record.routes.js
+++ routes/record.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const ctrl = require('../controllers/record.controller');
 
+router.post('/', ctrl.createRecord);
 router.post('/sync', ctrl.sync);
 router.get('/', ctrl.getAll);
 router.put('/:id', ctrl.update);
